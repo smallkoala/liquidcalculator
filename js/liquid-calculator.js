737032dddfe96eaaf90dbbb18855a851801b758a
Niedhammer <Niedhammer@elrest.zz>
2016-11-24 17:19:52 +0100

    Basic files added

---
+++ js/liquid-calculator.js
@@ -0,0 +1,41 @@
+/*!
+ * Liquid-Calculator (http://www.niced.de)
+ * Version: 0.1
+ * Copyright 2016 Cedrik Niedhammer.
+ * Licensed under the MIT license
+ */
+
+var	baseNico = 0;
+var	basePG = 0;
+var	baseVG = 0;
+var	baseWA = 0;
+var	basePrice = 0;
+var	baseQty = 0;
+var	pgPrice = 0;
+var	pgQty = 0;
+var	glycerinVG = 0;
+var	glycerinWA = 0;
+var	glycerinPrice = 0;
+var	glycerinQty = 0;
+var	targetNico = 0;
+var	targetPG = 0;
+var	targetVG = 0;
+var	targetWA = 0;
+var	targetQty = 0;
+var	numAromas = 0;
+var	Aromas           = new Array();
+var mlNeededBase = 0;
+var mlNeededPG = 0;
+var mlNeededVG = 0;
+var mlNeededWA = 0;
+var grNeededBase = 0;
+var grNeededPG   = 0;
+var grNeededVG   = 0;
+var grNeededWA   = 0;
+var targetBasePerc = 0;
+var targetPGPerc = 0;
+var targetVGPerc = 0;
+var targetWAPerc = 0;
+var costTotal = 0;
+var targetQty = 0;
+var totalAromaPerc = 0;
